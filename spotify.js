eb7a430b78692d005ef7f7135e74875226b5285f
Linda Foo <linda.foo.dev@gmail.com>
2017-12-21 17:17:50 -0800

    added comments from mentor review

--- spotify.js
+++ spotify.js
@@ -20,11 +20,11 @@ const getFromApi = function (endpoint, query = {}) {
     if (!response.ok) {
       return Promise.reject(response.statusText);
     }
-    return response.json();
+    return response.json(); //grabbing the json data from response object and returning whatever that value is 
   });
 };
 
-let artist;
+let artist = {}; //don't necessarily need to designate type, however, best practice is to do so. 
 
 const getArtist = function (name) {
   let query = {
@@ -33,18 +33,19 @@ const getArtist = function (name) {
     type: 'artist'
   };
   return getFromApi('search', query)
-    .then( (item) => {
+    .then( (item) => { //response returned (result of response.json())
       artist = item.artists.items[0];
       return getFromApi(`artists/${artist.id}/related-artists`);
-    })
-    .then( (item) => { 
-      artist.related = item.artists;
-      let concurrentTopTracks = [];
-      for (let i=0; i < artist.related.length; i++) {
+    }) //result of this getFromApi request
+    .then( (relatedArtist) => { 
+      artist.related = item.artists; //creating a new property on artist object, and the value is whatever artist you'll pull from the response - works because reference type rather than value type
+      let concurrentTopTracks = []; //artist.related.map; map gets rid of creating extra variable (lines of code) - more chances to fail
+      for (let i=0; i < artist.related.length; i++) { //can also (prefer map)artist.related
         concurrentTopTracks.push(getFromApi(`artists/${artist.related[i].id}/top-tracks`, { country : 'US' }));
-      }
-      return Promise.all(concurrentTopTracks); //a promise that resolves with an array of results from previous promises
-    })
+      } //getFromApi is a promise function, just returning promise here without executing
+      console.log('concurrentTopTracks', concurrentTopTracks); 
+      return Promise.all(concurrentTopTracks);//all children complete, then parent resolves; if any one promise fails, then whole parent promise will reject and other promises //promise all is where it gets executed //a promise that resolves with an array of results from previous promises
+    }) //result of getFromApi request (is a function, but request is more descriptive) is what gets pushed to concurrentTopTracks
     .then( (items) => {
       for (let i=0; i<items.length; i++){
         artist.related[i].tracks = items[i].tracks;
